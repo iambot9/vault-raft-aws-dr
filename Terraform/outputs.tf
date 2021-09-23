fa9840942359f3809abbacb186d2ac26cc1bd50e
iambot9 <aamalik1@outlook.com>
2021-09-22 23:47:09 -0400

    main.tf

--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -1,8 +1,8 @@
 output "endpoints" {
   value = {
 
-      for instance in aws_instance.vault-server:
-        instance.tags.instance_name => "public: ${instance.public_ip} | private: ${instance.private_ip} | ssh -l ubuntu ${instance.public_ip} -i ${var.key_name}.pem"
+    for instance in aws_instance.vault-server :
+    instance.tags.instance_name => "public: ${instance.public_ip} | private: ${instance.private_ip} | ssh -l ubuntu ${instance.public_ip} -i ${var.key_name}.pem"
   }
- 
+
 }
--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -14,32 +14,32 @@ variable "environment_name" {
 
 variable "vault_transit_private_ip" {
   description = "The private ip of the first Vault node for Auto Unsealing"
-  default = "10.0.101.21"
+  default     = "10.0.101.21"
 }
 
 
 variable "vault_server_names" {
   description = "Names of the Vault nodes that will join the cluster"
-  type = list(string)
-  default = [ "vault_1", "vault_2", "vault_3", "vault_1_DR", "vault_2_DR", "vault_3_DR" ]
+  type        = list(string)
+  default     = ["vault_1", "vault_2", "vault_3", "vault_1_DR", "vault_2_DR", "vault_3_DR"]
 }
 
 variable "vault_server_private_ips" {
   description = "The private ips of the Vault nodes that will join the cluster"
   # @see https://docs.aws.amazon.com/vpc/latest/userguide/VPC_Subnets.html
-  type = list(string)
-  default = [ "10.0.101.22", "10.0.101.23", "10.0.101.24", "10.0.101.25", "10.0.101.26", "10.0.101.27" ]
+  type    = list(string)
+  default = ["10.0.101.22", "10.0.101.23", "10.0.101.24", "10.0.101.25", "10.0.101.26", "10.0.101.27"]
 }
 
 variable "instance_names" {
-    type = map(string)
-    default = {
-    "vault_1": "10.0.101.22",
-    "vault_2": "10.0.101.23",
-    "vault_3": "10.0.101.24", 
-    "vault_1_DR": "10.0.101.25",
-    "vault_2_DR": "10.0.101.26",
-    "vault_3_DR": "10.0.101.27",
+  type = map(string)
+  default = {
+    "vault_1" : "10.0.101.22",
+    "vault_2" : "10.0.101.23",
+    "vault_3" : "10.0.101.24",
+    "vault_1_DR" : "10.0.101.25",
+    "vault_2_DR" : "10.0.101.26",
+    "vault_3_DR" : "10.0.101.27",
   }
 }
 
